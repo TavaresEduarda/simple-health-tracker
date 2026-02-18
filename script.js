4f4ec0dc9a5bc3d5097be90ba5d00571202940fa
TavaresEduarda <eduardaferreiratc@gmail.com>
2026-02-18 11:09:43 -0300

    feat: add functionality to status element

--- script.js
+++ script.js
@@ -8,11 +8,18 @@ btn.addEventListener('click', function() {
     height = height / 100;
 
     if (weight > 0 && height > 0) {
-        
-        
         const bmi = weight / (height *  height);
-    
         document.getElementById('bmi-value').innerText = bmi.toFixed(2);
+        
+        if (bmi < 18.5) {
+            document.getElementById('description').innerText = "Status: Underweight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            document.getElementById('description').innerText = "Status: Normal weight";
+        } else if (bmi >= 25 && bmi < 30) {
+            document.getElementById('description').innerText = "Status: Overweight";
+        } else {
+            document.getElementById('description').innerText = "Status: Obesity";
+        }
     } else{
         alert("Please fill in all fields!");
     }
